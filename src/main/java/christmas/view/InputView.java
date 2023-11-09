b4d2e8068a58f7e505109f6a221e8068c485b323
slowcloud <sju9417@gmail.com>
2023-11-09 23:08:20 +0900

    refactor: InputView에서 Today에 대한 종속성 제거

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,10 +5,9 @@ import christmas.domain.Today;
 
 public class InputView {
 
-    public Today getToday() {
+    public String getToday() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String number = Console.readLine();
-        return Today.of(number);
+        return Console.readLine();
     }
 
 }

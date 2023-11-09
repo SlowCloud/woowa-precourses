67339b54afc2dd10092bea702aca1859d52703f1
slowcloud <sju9417@gmail.com>
2023-11-10 00:07:41 +0900

    feat: OrderService 객체 생성

---
+++ src/main/java/christmas/service/OrderService.java
@@ -0,0 +1,43 @@
+package christmas.service;
+
+import christmas.domain.Order;
+
+public class OrderService {
+
+    public static final String DASH_STRING = "-";
+    public static final char DASH_CHAR = '-';
+    public static final int DASH_COUNT = 1;
+    public static final String INVALID_ORDER_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.";
+
+    public Order createOrder(String orderString) {
+        validateDash(orderString);
+        String[] splitedString = orderString.split(DASH_STRING);
+        return createOrder(splitedString[0].trim(), splitedString[1].trim());
+    }
+
+    private void validateDash(String orderString) {
+        if (getDashCount(orderString) != DASH_COUNT) {
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
+        }
+    }
+
+    private long getDashCount(String orderString) {
+        return orderString.chars()
+                .filter(value -> value == DASH_CHAR)
+                .count();
+    }
+
+    private Order createOrder(String menu, String count) {
+        validateNumber(count);
+        return new Order(menu, Integer.parseInt(count));
+    }
+
+    private void validateNumber(String count) {
+        try {
+            Integer.parseInt(count);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
+        }
+    }
+
+}

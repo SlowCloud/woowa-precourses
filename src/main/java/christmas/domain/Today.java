44c14f42f4e81a021bac0fde2d6e3fc615b25d34
slowcloud <sju9417@gmail.com>
2023-11-10 00:24:47 +0900

    refactor: Today의 매직 넘버를 상수로 이동

--- src/main/java/christmas/domain/Today.java
+++ src/main/java/christmas/domain/Today.java
@@ -2,6 +2,8 @@ package christmas.domain;
 
 public class Today {
 
+    public static final int START_DATE = 1;
+    public static final int LAST_DATE = 31;
     private final int today;
 
     public Today(int today) {
@@ -10,7 +12,7 @@ public class Today {
     }
 
     private void validateTodayRange(int today) {
-        if (today < 1 || 31 < today) {
+        if (today < START_DATE || LAST_DATE < today) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }

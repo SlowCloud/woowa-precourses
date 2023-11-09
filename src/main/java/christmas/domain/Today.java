b2ab0ce534c672c1bd7c2481be5cdfa28eb00dba
slowcloud <sju9417@gmail.com>
2023-11-09 21:35:26 +0900

    feat: Today 정적 팩토리 메소드 추가

--- src/main/java/christmas/domain/Today.java
+++ src/main/java/christmas/domain/Today.java
@@ -10,8 +10,21 @@ public class Today {
     }
 
     private void validateTodayRange(int today) {
-        if(today < 1 || 31 < today) {
+        if (today < 1 || 31 < today) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static Today of(String today) {
+        validateNumber(today);
+        return new Today(Integer.parseInt(today));
+    }
+
+    private static void validateNumber(String today) {
+        try {
+            Integer.parseInt(today);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력이 숫자가 아닙니다. 다시 입력해주세요.");
         }
     }
 

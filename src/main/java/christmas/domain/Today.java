7a832b14a4e74f0f00653ac644dca247510e9259
slowcloud <sju9417@gmail.com>
2023-11-09 21:06:46 +0900

    feat: Today 객체 추가

---
+++ src/main/java/christmas/domain/Today.java
@@ -0,0 +1,18 @@
+package christmas.domain;
+
+public class Today {
+
+    private final int today;
+
+    public Today(int today) {
+        validateTodayRange(today);
+        this.today = today;
+    }
+
+    private void validateTodayRange(int today) {
+        if(today < 1 || 31 < today) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+}

10466535be6adabef79928794776c9ea503ec039
slowcloud <sju9417@gmail.com>
2023-11-10 00:55:56 +0900

    feat(Application): ChristmasController 구성 후 실행하는 코드 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,28 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.service.OrderService;
+import christmas.service.TodayService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        TodayService todayService = new TodayService();
+        OrderService orderService = new OrderService();
+
+        ChristmasController christmasController = new ChristmasController(
+                inputView,
+                outputView,
+                orderService,
+                todayService
+        );
+
+        christmasController.play();
+
     }
 }

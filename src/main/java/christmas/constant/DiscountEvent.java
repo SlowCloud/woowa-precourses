f0561c85cf62804e0ab09d1db3abbfcbc4c84d68
slowcloud <sju9417@gmail.com>
2023-11-10 22:47:24 +0900

    feat(DiscountEvent): 이벤트 이름 추가

--- src/main/java/christmas/constant/DiscountEvent.java
+++ src/main/java/christmas/constant/DiscountEvent.java
@@ -1,6 +1,5 @@
 package christmas.constant;
 
-import christmas.domain.DiscountEvents;
 import christmas.domain.Today;
 
 import java.util.Arrays;
@@ -9,14 +8,28 @@ import java.util.function.Function;
 
 public enum DiscountEvent {
 
-    CHRISTMAS_D_DAY(today -> today.between(1, 25)),
-    WEEKDAY(Today::isWeekday),
-    WEEKEND(Today::isWeekend),
-    SPECIAL(today -> today.isChristmas() || today.isSunday());
+    CHRISTMAS_D_DAY(
+            "크리스마스 디데이 할인",
+            today -> today.between(1, 25)
+    ),
+    WEEKDAY(
+            "평일 할인",
+            Today::isWeekday
+    ),
+    WEEKEND(
+            "주말 할인",
+            Today::isWeekend
+    ),
+    SPECIAL(
+            "특별 할인",
+            today -> today.isChristmas() || today.isSunday()
+    );
 
+    private final String eventName;
     private final Function<Today, Boolean> validator;
 
-    DiscountEvent(Function<Today, Boolean> validator) {
+    DiscountEvent(String eventName, Function<Today, Boolean> validator) {
+        this.eventName = eventName;
         this.validator = validator;
     }
 

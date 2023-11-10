611c4c4a5478f6b8122065e2ce2fa384889332ae
slowcloud <sju9417@gmail.com>
2023-11-10 23:22:39 +0900

    feat(GiveawayEvent): getGiveawayMessage 생성

--- src/main/java/christmas/constant/GiveawayEvent.java
+++ src/main/java/christmas/constant/GiveawayEvent.java
@@ -10,22 +10,30 @@ public enum GiveawayEvent {
 
     CHAMPAGNE_GIVEAWAY(
             price -> price >= 120_000,
-            Menu.CHAMPAGNE
+            Menu.CHAMPAGNE,
+            1
     );
 
     public static final String GIVEAWAY_DISCOUNT_MESSAGE = "증정 이벤트";
+    public static final String GIVEAWAY_MESSAGE_TEMPLATE = "%s %d개";
     private final Function<Integer, Boolean> validator;
     private final Menu menu;
+    private final int count;
 
-    GiveawayEvent(Function<Integer, Boolean> validator, Menu menu) {
+    GiveawayEvent(Function<Integer, Boolean> validator, Menu menu, int count) {
         this.validator = validator;
         this.menu = menu;
+        this.count = count;
     }
 
     public static List<GiveawayEvent> getAvailableEvents(int price) {
         return Arrays.stream(values())
                 .filter(giveawayEvent -> giveawayEvent.validator.apply(price))
                 .toList();
+    }
+
+    public String getGiveawayMessage() {
+        return String.format(GIVEAWAY_MESSAGE_TEMPLATE, menu.getMenuName(), count);
     }
 
     public Discount getDiscount() {

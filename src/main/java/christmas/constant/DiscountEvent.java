33c1c0389f65173d30d5f9ea9c80f1f3ff285c12
slowcloud <sju9417@gmail.com>
2023-11-10 20:49:46 +0900

    feat(DiscountEvent): DiscountEvent 객체 생성

---
+++ src/main/java/christmas/constant/DiscountEvent.java
@@ -0,0 +1,23 @@
+package christmas.constant;
+
+import christmas.domain.DiscountEvents;
+import christmas.domain.Today;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public enum DiscountEvent {
+
+    CHRISTMAS_D_DAY(today -> today.between(1, 25)),
+    WEEKDAY(Today::isWeekday),
+    WEEKEND(Today::isWeekend),
+    SPECIAL(today -> today.isChristmas() || today.isSunday());
+
+    private final Function<Today, Boolean> validator;
+
+    DiscountEvent(Function<Today, Boolean> validator) {
+        this.validator = validator;
+    }
+
+}

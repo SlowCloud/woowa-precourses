22ab6376d3ce597029f9461d0e696b1a092fcfb4
slowcloud <sju9417@gmail.com>
2023-11-10 20:50:08 +0900

    feat(DiscountEvents): DiscountEvents 일급 컬렉션 생성

---
+++ src/main/java/christmas/domain/DiscountEvents.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+import christmas.constant.DiscountEvent;
+
+import java.util.List;
+
+public class DiscountEvents {
+
+    private final List<DiscountEvent> discountEvents;
+
+    public DiscountEvents(List<DiscountEvent> discountEvents) {
+        this.discountEvents = discountEvents;
+    }
+
+}

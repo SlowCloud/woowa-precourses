6676f9ad60f619a7b2e3150dbfef2a12bf3945b9
slowcloud <sju9417@gmail.com>
2023-11-10 23:17:13 +0900

    feat(GiveawayEvent): getDiscount 메소드 생성

--- src/main/java/christmas/constant/GiveawayEvent.java
+++ src/main/java/christmas/constant/GiveawayEvent.java
@@ -1,23 +1,35 @@
 package christmas.constant;
 
+import christmas.domain.Discount;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 public enum GiveawayEvent {
 
     CHAMPAGNE_GIVEAWAY(
-            price -> price >= 120_000
+            price -> price >= 120_000,
+            Menu.CHAMPAGNE
     );
 
+    public static final String GIVEAWAY_DISCOUNT_MESSAGE = "증정 이벤트";
     private final Function<Integer, Boolean> validator;
+    private final Menu menu;
 
-    GiveawayEvent(Function<Integer, Boolean> validator) {
+    GiveawayEvent(Function<Integer, Boolean> validator, Menu menu) {
         this.validator = validator;
+        this.menu = menu;
     }
 
     public static List<GiveawayEvent> getAvailableEvents(int price) {
         return Arrays.stream(values())
                 .filter(giveawayEvent -> giveawayEvent.validator.apply(price))
                 .toList();
+    }
+
+    public Discount getDiscount() {
+        return new Discount(GIVEAWAY_DISCOUNT_MESSAGE, menu.getPrice());
     }
 
 }

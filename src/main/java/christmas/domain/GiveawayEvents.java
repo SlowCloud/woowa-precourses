98acf4b30844654a9b6775ba221e1b120d48c34b
slowcloud <sju9417@gmail.com>
2023-11-10 23:19:28 +0900

    refactor(GiveawayEvents): 객체 생성

---
+++ src/main/java/christmas/domain/GiveawayEvents.java
@@ -0,0 +1,17 @@
+package christmas.domain;
+
+import christmas.constant.GiveawayEvent;
+
+import java.util.List;
+
+public class GiveawayEvents {
+
+    private final List<GiveawayEvent> giveawayEvents;
+
+    public GiveawayEvents(List<GiveawayEvent> giveawayEvents) {
+        this.giveawayEvents = giveawayEvents;
+    }
+
+
+
+}

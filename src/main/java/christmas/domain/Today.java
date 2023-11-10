a1f207de1f986ef85ac6a867023a7d72e5eacdc2
slowcloud <sju9417@gmail.com>
2023-11-10 18:43:31 +0900

    feat(Today): 날짜를 확인하는 메소드 생성
    - between
    - isWeekday
    - isWeekend
    - isChristmas
    - isSunday

--- src/main/java/christmas/domain/Today.java
+++ src/main/java/christmas/domain/Today.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import christmas.constant.Calender;
+
 public class Today {
 
     public static final int START_DATE = 1;
@@ -16,5 +18,26 @@ public class Today {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
+
+    public boolean between(int from, int to) {
+        return from <= today && today <= to;
+    }
+
+    public boolean isWeekday() {
+        return Calender.WEEKDAY.validate(today);
+    }
+
+    public boolean isWeekend() {
+        return Calender.WEEKEND.validate(today);
+    }
+
+    public boolean isChristmas() {
+        return Calender.CHRISTMAS.validate(today);
+    }
+
+    public boolean isSunday() {
+        return Calender.SUNDAY.validate(today);
+    }
+
 
 }

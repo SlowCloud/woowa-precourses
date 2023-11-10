0ae8a4666fc437667dc0343d7feccb8e87210e7b
slowcloud <sju9417@gmail.com>
2023-11-10 23:00:45 +0900

    feat(Orders): Discount를 반환하는 메소드 추가

--- src/main/java/christmas/constant/DiscountEvent.java
+++ src/main/java/christmas/constant/DiscountEvent.java
@@ -1,42 +1,59 @@
 package christmas.constant;
 
+import christmas.domain.Discount;
+import christmas.domain.Orders;
 import christmas.domain.Today;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public enum DiscountEvent {
 
     CHRISTMAS_D_DAY(
             "크리스마스 디데이 할인",
-            today -> today.between(1, 25)
+            today -> today.between(1, 25),
+            (today, orders) -> 1000L + today.getToday() * 100L
     ),
     WEEKDAY(
             "평일 할인",
-            Today::isWeekday
+            Today::isWeekday,
+            (today, orders) -> orders.getCourseCount(Course.DESSERT) * 2023
     ),
     WEEKEND(
             "주말 할인",
-            Today::isWeekend
+            Today::isWeekend,
+            (today, orders) -> orders.getCourseCount(Course.MAIN) * 2023
     ),
     SPECIAL(
             "특별 할인",
-            today -> today.isChristmas() || today.isSunday()
+            today -> today.isChristmas() || today.isSunday(),
+            (today, orders) -> 1000L
     );
 
     private final String eventName;
     private final Function<Today, Boolean> validator;
+    private final BiFunction<Today, Orders, Long> discountCalculator;
 
-    DiscountEvent(String eventName, Function<Today, Boolean> validator) {
+    DiscountEvent(
+            String eventName,
+            Function<Today, Boolean> validator,
+            BiFunction<Today, Orders, Long> discountCalculator
+    ) {
         this.eventName = eventName;
         this.validator = validator;
+        this.discountCalculator = discountCalculator;
     }
 
     public static List<DiscountEvent> getAvailableEvents(Today today) {
         return Arrays.stream(DiscountEvent.values())
                 .filter(discountEvent -> discountEvent.validator.apply(today))
                 .toList();
+    }
+
+    public Discount getDiscount(Today today, Orders orders) {
+        return new Discount(eventName, discountCalculator.apply(today, orders));
     }
 
 }

947027b8cec66c7224d51e37394bbecb84acc830
slowcloud <sju9417@gmail.com>
2023-11-10 23:35:03 +0900

    feat(GiveawayEvents): getGiveawayMessage 생성

--- src/main/java/christmas/domain/GiveawayEvents.java
+++ src/main/java/christmas/domain/GiveawayEvents.java
@@ -3,15 +3,29 @@ package christmas.domain;
 import christmas.constant.GiveawayEvent;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class GiveawayEvents {
 
+    public static final String NON_GIVEAWAY_MESSAGE = "없음";
     private final List<GiveawayEvent> giveawayEvents;
 
     public GiveawayEvents(List<GiveawayEvent> giveawayEvents) {
         this.giveawayEvents = giveawayEvents;
     }
 
+    public String getGiveawayMessage() {
 
+        if (giveawayEvents.isEmpty()) {
+            return NON_GIVEAWAY_MESSAGE;
+        }
+
+        List<String> messages = giveawayEvents.stream()
+                .map(GiveawayEvent::getGiveawayMessage)
+                .toList();
+
+        return String.join("\n", messages);
+
+    }
 
 }

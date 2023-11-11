9d63ed6c3ebc482af95b162968a11c9fef515c18
slowcloud <sju9417@gmail.com>
2023-11-11 16:39:57 +0900

    refactor(Discounts): add 메소드 제거, 생성자에서 List<Discount>를 받도록 변경

--- src/main/java/christmas/domain/Discount/Discounts.java
+++ src/main/java/christmas/domain/Discount/Discounts.java
@@ -1,18 +1,13 @@
 package christmas.domain.Discount;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Discounts {
 
     private final List<Discount> discounts;
 
-    public Discounts() {
-        this.discounts = new ArrayList<>();
-    }
-
-    public void add(List<Discount> discounts) {
-        this.discounts.addAll(discounts);
+    public Discounts(List<Discount> discounts) {
+        this.discounts = discounts;
     }
 
     public String getDiscountMessage() {

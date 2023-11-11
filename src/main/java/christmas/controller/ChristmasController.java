e4a65af9ad7db86814ff4d4507755ca03ea89d5b
slowcloud <sju9417@gmail.com>
2023-11-11 23:07:29 +0900

    refactor: play 내 코드를 메소드로 분리

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -4,7 +4,6 @@ import christmas.constant.Badge;
 import christmas.constant.DiscountEvent;
 import christmas.constant.GiveawayEvent;
 import christmas.domain.Discount.Discounts;
-import christmas.domain.Discount.DiscountsBuilder;
 import christmas.domain.DiscountEvents;
 import christmas.domain.GiveawayEvents;
 import christmas.domain.Order.Orders;
@@ -50,22 +49,9 @@ public class ChristmasController {
         Discounts discountEventDiscounts = new Discounts(discountEvents.getDiscounts(today, orders));
         Discounts giveawayDiscounts = new Discounts(giveawayEvents.getDiscounts());
 
-        Badge badge = Badge.findBadge(
-                discountEventDiscounts.getTotalDiscounts() + giveawayDiscounts.getTotalDiscounts()
-        );
+        Badge badge = getBadge(discountEventDiscounts, giveawayDiscounts);
 
-        outputView.printEventPreviewMessage();
-        outputView.printOrderedMenus(orders.getOrderedMenusMessage());
-        outputView.printTotalPriceBeforeDiscount(orders.getTotalPrice());
-        outputView.printGiveaway(giveawayEvents.getGiveawayMessage());
-        outputView.printDiscounts(
-                discountEventDiscounts.getDiscountMessage(), giveawayDiscounts.getDiscountMessage()
-        );
-        outputView.printTotalDiscounts(
-                discountEventDiscounts.getTotalDiscounts() + giveawayDiscounts.getTotalDiscounts()
-        );
-        outputView.printTotalPriceAfterDiscount(orders.getTotalPrice() + discountEventDiscounts.getTotalDiscounts());
-        outputView.printBadge(badge.getBadgeName());
+        printReceipt(orders, giveawayEvents, discountEventDiscounts, giveawayDiscounts, badge);
 
     }
 
@@ -87,6 +73,27 @@ public class ChristmasController {
         return new DiscountEvents(
                 DiscountEvent.getAvailableEvents(today, orders.getTotalPrice())
         );
+    }
+
+    private static Badge getBadge(Discounts discountEventDiscounts, Discounts giveawayDiscounts) {
+        return Badge.findBadge(
+                discountEventDiscounts.getTotalDiscounts() + giveawayDiscounts.getTotalDiscounts()
+        );
+    }
+
+    private void printReceipt(Orders orders, GiveawayEvents giveawayEvents, Discounts discountEventDiscounts, Discounts giveawayDiscounts, Badge badge) {
+        outputView.printEventPreviewMessage();
+        outputView.printOrderedMenus(orders.getOrderedMenusMessage());
+        outputView.printTotalPriceBeforeDiscount(orders.getTotalPrice());
+        outputView.printGiveaway(giveawayEvents.getGiveawayMessage());
+        outputView.printDiscounts(
+                discountEventDiscounts.getDiscountMessage(), giveawayDiscounts.getDiscountMessage()
+        );
+        outputView.printTotalDiscounts(
+                discountEventDiscounts.getTotalDiscounts() + giveawayDiscounts.getTotalDiscounts()
+        );
+        outputView.printTotalPriceAfterDiscount(orders.getTotalPrice() + discountEventDiscounts.getTotalDiscounts());
+        outputView.printBadge(badge.getBadgeName());
     }
 
     private <T> T tryCatchLoop(Supplier<T> supplier) {

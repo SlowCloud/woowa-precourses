00b55f13da77e6668e977d8f65e1b51a876ca56e
slowcloud <sju9417@gmail.com>
2023-11-11 13:08:55 +0900

    fix(Discount): 문자열 생성 오류를 해결

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -5,7 +5,7 @@ public class Discount {
     private final String message;
 
     public Discount(String discountMessage, long discount) {
-        this.message = String.format(discountMessage + ": -%,l원", discount);
+        this.message = String.format(discountMessage + ": -%,d원", discount);
     }
 
     public String getMessage() {

3487b66e7b82c047a2b9f385f02765af3418336e
slowcloud <sju9417@gmail.com>
2023-11-11 20:54:02 +0900

    refactor(Calender): 람다식을 짧게 수정

--- src/main/java/christmas/constant/Calender.java
+++ src/main/java/christmas/constant/Calender.java
@@ -6,9 +6,13 @@ public enum Calender {
 
     BEFORE_CHRISTMAS(day -> day <= Constants.CHRISTMAS_DAY),
     CHRISTMAS(day -> day == Constants.CHRISTMAS_DAY),
-    WEEKDAY(day -> day % 7 == Constants.FRIDAY || day % 7 == Constants.SATURDAY),
-    WEEKEND(day -> day % 7 == Constants.SUNDAY || day % 7 == Constants.MONDAY),
-    SUNDAY(day -> day % 7 == Constants.SUNDAY);
+    WEEKDAY(day -> isDayOfWeek(day, Constants.FRIDAY) || isDayOfWeek(day, Constants.SATURDAY)),
+    WEEKEND(day -> isDayOfWeek(day, Constants.SUNDAY) || isDayOfWeek(day, Constants.MONDAY)),
+    SUNDAY(day -> isDayOfWeek(day, Constants.SUNDAY));
+
+    private static boolean isDayOfWeek(Integer day, int dayOfWeek) {
+        return day % Constants.WEEK_LENGTH == dayOfWeek;
+    }
 
     private final Function<Integer, Boolean> validator;
 
@@ -26,6 +30,7 @@ public enum Calender {
         public static final int SATURDAY = 2;
         public static final int SUNDAY = 3;
         public static final int MONDAY = 4;
+        public static final int WEEK_LENGTH = 7;
     }
 
 }

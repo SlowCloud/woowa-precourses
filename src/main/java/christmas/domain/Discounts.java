f90e0abe62c02d0637b02157fbd733096d43e428
slowcloud <sju9417@gmail.com>
2023-11-11 13:05:05 +0900

    refactor(Discounts): add가 List<Discount>를 입력받도록 변경, 생성자에 인자가 없도록 변경

--- src/main/java/christmas/domain/Discounts.java
+++ src/main/java/christmas/domain/Discounts.java
@@ -1,13 +1,18 @@
 package christmas.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Discounts {
 
     private final List<Discount> discounts;
 
-    public Discounts(List<Discount> discounts) {
-        this.discounts = discounts;
+    public Discounts() {
+        this.discounts = new ArrayList<>();
+    }
+
+    public void add(List<Discount> discounts) {
+        this.discounts.addAll(discounts);
     }
 
     private String getDiscountMessage() {
@@ -15,10 +20,6 @@ public class Discounts {
                 .map(Discount::getMessage)
                 .toList();
         return String.join("\n", messages);
-    }
-
-    public void add(Discounts discounts) {
-        this.discounts.addAll(discounts.discounts);
     }
 
 }

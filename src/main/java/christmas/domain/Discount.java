503de46c3892d7b4a70575e416bf9e074906cd29
slowcloud <sju9417@gmail.com>
2023-11-11 13:37:16 +0900

    feat(Discount): getDiscount 메소드 추가

--- src/main/java/christmas/domain/Discount.java
+++ src/main/java/christmas/domain/Discount.java
@@ -3,13 +3,19 @@ package christmas.domain;
 public class Discount {
 
     private final String message;
+    private final int discount;
 
-    public Discount(String discountMessage, long discount) {
+    public Discount(String discountMessage, int discount) {
+        this.discount = discount;
         this.message = String.format(discountMessage + ": -%,d원", discount);
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public int getDiscount() {
+        return discount;
     }
 
 }

ef3226804781862803943e01414fdd297eaa27c6
slowcloud <sju9417@gmail.com>
2023-11-12 12:40:10 +0900

    feat(ChampagneGiveawayEvent): 객체 생성

---
+++ src/main/java/christmas/domain/Event/ChampagneGiveawayEvent.java
@@ -0,0 +1,34 @@
+package christmas.domain.Event;
+
+import christmas.constant.Menu;
+import christmas.domain.Discount.Discount;
+
+public class ChampagneGiveawayEvent implements GiveawayEvent {
+
+    private final Menu champagne = Menu.CHAMPAGNE;
+    private final int count = 1;
+
+    public ChampagneGiveawayEvent(int price) {
+        validatePrice(price);
+    }
+
+    private void validatePrice(int price) {
+        if(price < 120_000) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static Event createInstance(int price) {
+        try {
+            return new ChampagneGiveawayEvent(price);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public Discount getDiscount() {
+        return new Discount("증정 이벤트", champagne.getPrice());
+    }
+
+}

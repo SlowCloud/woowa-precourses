425eb2fa6f66eea807e4b6a8050a1a14480b7e2a
slowcloud <sju9417@gmail.com>
2023-12-05 15:22:31 +0900

    feat: BridgeMaker 구현

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,6 +1,13 @@
 package bridge;
 
+import org.assertj.core.configuration.Services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
@@ -18,6 +25,12 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        Map<Integer, String> bridgeConvert = new HashMap<>();
+        bridgeConvert.put(0, "U");
+        bridgeConvert.put(1, "D");
+        return IntStream.range(0,size)
+                .map(value -> bridgeNumberGenerator.generate())
+                .mapToObj(bridgeConvert::get)
+                .collect(Collectors.toList());
     }
 }

cb2076d49b4adf1e540658337330c3fd5174ed8d
slowcloud <sju9417@gmail.com>
2023-12-05 16:51:42 +0900

    refactor: import 최적화

--- src/test/java/bridge/constants/MoveTest.java
+++ src/test/java/bridge/constants/MoveTest.java
@@ -9,21 +9,21 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-class GameCommandTest {
+class MoveTest {
     @Nested
     class findByName {
         @DisplayName("없는 이름을 제시하면 오류를 던진다.")
         @Test
         void givenNameIsNotExist() {
-            Assertions.assertThatThrownBy(() -> GameCommand.findByName("없음!!"))
+            Assertions.assertThatThrownBy(() -> Move.findByName("없음!!"))
                     .isInstanceOf(IllegalArgumentException.class);
         }
 
         @DisplayName("올바른 이름을 제시하면 통과한다.")
         @ParameterizedTest
-        @ValueSource(strings = {"Q", "R"})
-        void givenNameIsExist(String command) {
-            assertDoesNotThrow(() -> GameCommand.findByName(command));
+        @ValueSource(strings = {"U", "D"})
+        void givenNameIsExist(String move) {
+            assertDoesNotThrow(() -> Move.findByName(move));
         }
     }
 }

1a010817cf7a3aa6310e3a9d75f60f01bbbf75b4
slowcloud <sju9417@gmail.com>
2023-12-05 16:58:08 +0900

    test:BridgeMakerTest 구현

---
+++ src/test/java/bridge/BridgeMakerTest.java
@@ -0,0 +1,33 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeMakerTest {
+    @Nested
+    class makeBridge {
+        @DisplayName("길이에 맞게 의도대로 다리가 잘 생성되는지 확인한다.")
+        @Test
+        void testMakeBridge() {
+            BridgeMaker bridgeMaker =new BridgeMaker(new FakeBridgeNumberGenerator());
+            List<String> bridge=  bridgeMaker.makeBridge(10);
+            assertAll(
+                    () -> assertEquals(bridge.size(), 10),
+                    () -> Assertions.assertThat(bridge).allMatch(s -> s.equals("D"))
+            );
+        }
+    }
+}
+
+class FakeBridgeNumberGenerator implements BridgeNumberGenerator {
+    @Override
+    public int generate() {
+        return 0;
+    }
+}

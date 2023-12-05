43f8cfade76df24abd6fa6fc449bf65686b6acc3
slowcloud <sju9417@gmail.com>
2023-12-05 16:49:08 +0900

    test: GameCommandTest 작성

---
+++ src/test/java/bridge/constants/GameCommandTest.java
@@ -0,0 +1,30 @@
+package bridge.constants;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameCommandTest {
+    @Nested
+    class findByName {
+        @DisplayName("없는 이름을 제시하면 오류를 던진다.")
+        @Test
+        void givenNameIsNotExist() {
+            Assertions.assertThatThrownBy(() -> GameCommand.findByName("없음!!"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("올바른 이름을 제시하면 통과한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"Q", "R"})
+        void givenNameIsExist(String command) {
+            assertDoesNotThrow(() -> GameCommand.findByName(command));
+        }
+    }
+
+}

818ed52441b1103af91db95f080933a4ecf9bc60
slowcloud <sju9417@gmail.com>
2023-12-21 18:06:21 +0900

    feat: 이름 제한사항 구현

--- src/main/java/oncall/domain/Worker.java
+++ src/main/java/oncall/domain/Worker.java
@@ -1,4 +1,13 @@
 package oncall.domain;
 
 public record Worker(String name) {
+    public Worker {
+        validateName(name);
+    }
+
+    private void validateName(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+        }
+    }
 }

438a2beb5b2eeaae7233ac96779b6e91e2d9dfa1
slowcloud <sju9417@gmail.com>
2023-12-21 17:35:09 +0900

    feat: InputView 구현

--- src/main/java/oncall/domain/Day.java
+++ src/main/java/oncall/domain/Day.java
@@ -1,7 +1,6 @@
 package oncall.domain;
 
 import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.time.Month;
 
 public class Day {
@@ -13,10 +12,14 @@ public class Day {
         this(month, dayOfWeek, 1);
     }
 
-    private Day(Month month,  DayOfWeek dayOfWeek, int today) {
+    private Day(Month month, DayOfWeek dayOfWeek, int today) {
         this.dayOfWeek = dayOfWeek;
         this.month = month;
         this.today = today;
+    }
+
+    public static Day of(String line) {
+        return Converter.getDay(line);
     }
 
     public boolean end() {
@@ -29,5 +32,47 @@ public class Day {
 
     public Day next() {
         return new Day(month, dayOfWeek.plus(1), today + 1);
+    }
+
+    // 서비스로 빼기. 다량의 테스트 필요
+    private static class Converter {
+
+        private static final String DAY_DELIMITER = ",";
+
+        public static Day getDay(String line) {
+            String[] split = line.split(DAY_DELIMITER);
+            return getDay(split[0].trim(), split[1].trim());
+        }
+
+        private static Day getDay(String month, String dayOfWeek) {
+            validateNumber(month);
+            return new Day(Month.of(Integer.parseInt(month)), DayOfWeekKor.find(dayOfWeek));
+        }
+
+        private static void validateNumber(String month) {
+            try {
+                Integer.parseInt(month);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+            }
+        }
+
+        private enum DayOfWeekKor {
+            월(1), 화(2), 수(3), 목(4), 금(5), 토(6), 일(7);
+
+            private final DayOfWeek dayOfWeek;
+
+            DayOfWeekKor(int number) {
+                this.dayOfWeek = DayOfWeek.of(number);
+            }
+
+            public static DayOfWeek find(String dayOfWeek) {
+                try {
+                    return valueOf(dayOfWeek).dayOfWeek;
+                } catch (IllegalArgumentException e) {
+                    throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+                }
+            }
+        }
     }
 }
--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,18 +1,33 @@
 package oncall.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import oncall.domain.Day;
 import oncall.domain.Workers;
 
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
     public static Day getDay() {
-        return null;
+        System.out.println("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+        String line = Console.readLine();
+        return Day.of(line);
     }
 
     public static Workers getWeekdayWorkers() {
-        return null;
+        System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요 >");
+        return getWorkers();
     }
 
     public static Workers getWeekendWorkers() {
-        return null;
+        System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요 >");
+        return getWorkers();
+    }
+
+    private static Workers getWorkers() {
+        String line = Console.readLine();
+        return new Workers(Arrays.stream(line.split(",")).toList());
     }
 }

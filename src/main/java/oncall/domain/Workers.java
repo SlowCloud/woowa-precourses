d2f871bb1bded7a8c83f905980bda73a0d60a53f
slowcloud <sju9417@gmail.com>
2023-12-21 15:54:42 +0900

    feat: workers 구현

--- src/main/java/oncall/domain/Workers.java
+++ src/main/java/oncall/domain/Workers.java
@@ -1,17 +1,27 @@
 package oncall.domain;
 
+import java.util.*;
+
 public class Workers {
+    Deque<Worker> workers;
+
+    public Workers(List<String> workers) {
+        this.workers = new LinkedList<>(workers.stream().map(Worker::new).toList());
+    }
+
     public boolean empty() {
-        return false;
+        return workers.isEmpty();
     }
 
     public Worker top() {
-        return null;
+        return workers.peekFirst();
     }
 
     public void pop() {
+        workers.poll();
     }
 
     public void pushFront(Worker worker) {
+        workers.addFirst(worker);
     }
 }

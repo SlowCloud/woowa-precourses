7e475dab1004a848b193bdd83f56c89add12fecd
slowcloud <sju9417@gmail.com>
2023-12-22 21:50:28 +0900

    feat: 이름 중복을 검사하도록 구현

--- src/main/java/oncall/domain/Workers.java
+++ src/main/java/oncall/domain/Workers.java
@@ -6,7 +6,15 @@ public class Workers {
     Deque<Worker> workers;
 
     public Workers(List<String> workers) {
+        validateDuplication(workers);
         this.workers = new LinkedList<>(workers.stream().map(Worker::new).toList());
+    }
+
+    private static void validateDuplication(List<String> workers) {
+        Set<String> set = new HashSet<>(workers);
+        if(set.size() != workers.size()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+        }
     }
 
     public boolean empty() {

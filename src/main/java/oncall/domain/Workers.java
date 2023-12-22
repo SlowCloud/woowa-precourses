17c7c89ca4039cc59251b3f293dad40c4506b0b2
slowcloud <sju9417@gmail.com>
2023-12-22 21:52:41 +0900

    feat: 근무자 수를 검사하도록 구현
    - 이게 총 근무자의 수를 따지는 건지, 순번 내 근무자의 수를 따지는 건지 잘 모르겠음. 일단 후자로 구현

--- src/main/java/oncall/domain/Workers.java
+++ src/main/java/oncall/domain/Workers.java
@@ -6,13 +6,24 @@ public class Workers {
     Deque<Worker> workers;
 
     public Workers(List<String> workers) {
-        validateDuplication(workers);
+        validateWorkers(workers);
         this.workers = new LinkedList<>(workers.stream().map(Worker::new).toList());
+    }
+
+    private void validateWorkers(List<String> workers) {
+        validateLength(workers);
+        validateDuplication(workers);
+    }
+
+    private void validateLength(List<String> workers) {
+        if (workers.size() < 5 || 35 < workers.size()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+        }
     }
 
     private static void validateDuplication(List<String> workers) {
         Set<String> set = new HashSet<>(workers);
-        if(set.size() != workers.size()) {
+        if (set.size() != workers.size()) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
         }
     }
